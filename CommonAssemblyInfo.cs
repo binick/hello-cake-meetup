587182faa147a803c5c7e6a8bccdb809489330d0
Nicola Biancolini <nicola.biancolini@reactconsulting.it>
2019-05-21 23:25:10 +0200

    fix download installer

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
-// Copyright (c) React Cosulting S.r.l., 2019. All rights reserved.
+// Copyright (c) Nicola Biancolini, 2019. All rights reserved.
 // Licensed under the MIT license. See the LICENSE file in the project root for full license information.
 
 using System;
